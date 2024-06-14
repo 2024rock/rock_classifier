0c149f9a4a9f002a567b14a623c06b78b5341fbb
2024rock <project2020tk@gmail.com>
2024-06-14 18:21:40 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+from PIL import Image
+from model import predict
+st.set_option("deprecation.showfileUploaderEncoding",False)
+st.sidebar.title("岩石標本の打音グラフ画像認識アプリ")#岩石標本の打音グラフ画像認識アプリ
+st.sidebar.write("岩石標本の打音データをグラフ化した画像データを学習したモデルで岩石の判定を行います")#岩石標本の打音データをグラフ化した画像データを学習したモデルで岩石の判定を行います
+
+st.sidebar.write("")
+
+
+#img_source = st.sidebar.radio("画像のソースを選択してください。","画像をアップロード")
+#if img_source == "画像をアップロード":
+
+img_file = st.sidebar.file_uploader("画像を選択してください。",type=["png","jpg","jpeg"])
+
+if img_file is not None:
+    with st.spinner("判定中"):
+        img = Image.open(img_file)
+        st.image(img, caption="対象の画像", width=480)
+        st.write("")
+
+        #予測
+        results = predict(img)
+
+        #結果の表示
+        st.subheader("判定結果")
+        n_top = 3#確率が高い順に3つまで返す
+        for result in results[:n_top]:
+            st.write(str(round(result[2]*100,2)) + "%の確率で" + result[0] + "です。")
+
+        #円グラフの表示
+        pie_labels = [result[1] for result in results[:n_top]]
+        pie_labels.append("others")#その他
+        pie_probs = [result[2] for result in results[:n_top]]
+        pie_probs.append(sum([result[2] for result in results[n_top:]])) #その他
+        fig, ax = plt.subplots()
+        wedgeprops={"width":0.3, "edgecolor":"white"}
+        textprops = {"fontsize":6}
+        ax.pie(pie_probs, labels=pie_labels, counterclock=False, startangle=90,textprops=textprops, autopct="%.2f",wedgeprops=wedgeprops) #円グラフ
+        st.pyplot(fig)
+
+        st.sidebar.write("")
+        st.sidebar.write("")
---
+++ model.py
@@ -0,0 +1,73 @@
+from PIL import Image
+
+from keras import models
+from keras.models import model_from_json
+from keras.preprocessing import image
+import numpy as np
+import os, glob
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import models, transforms
+
+classes_ja = ["ソーダ流紋岩","安山岩", "粗粒玄武岩"]
+classes_en = ["rhyolite_rock","andesite_rock", "xuanwu_rock"]
+n_class = len(classes_ja)
+img_size=(100,180,3)#(100,180,3)
+
+#画像認識のモデル
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3,8,3)
+        self.conv2 = nn.Conv2d(8,16,3)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.conv3 = nn.Conv2d(16,32,3)
+        self.conv4 = nn.Conv2d(32,64,3)
+        self.bn2 = nn.BatchNorm2d(64)
+
+        self.pool = nn.MaxPool2d(2,2)
+        self.relu = nn.ReLU()
+
+        self.fc1 = nn.Linear(193600, 256)#(64*4*4,256)
+        self.dropout = nn.Dropout(p=0.5)
+        self.fc2 = nn.Linear(256,3)#(256,10)
+
+    def forward(self, x):
+        x = self.relu(self.conv1(x))
+        x = self.relu(self.bn1(self.conv2(x)))
+        x = self.pool(x)
+        #x = F.relu(self.conv3(x))
+        #x = F.relu(self.bn2(self.conv4(x)))
+        #x = self.pool(x)
+        x = x.view(x.size(0), -1)#(-1, 64*4*4)
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.fc2(x)
+        return x
+
+net = Net()
+
+#訓練済みパラメータの読み込みと設定
+net.load_state_dict(torch.load("model_cnn_rock_3_Frequency_2.pth"))#,map_location=torch.device("cpu"))#"/content/drive/MyDrive/nose/cnn_pytorch//model_cnn.pth"
+net.to(torch.device("cpu"))
+
+def predict(img):
+    #モデルへの入力
+    img = img.resize((224,224))#サイズを変換img_size, img_size(100,180)
+    #img = img.convert("L")#モノクロに変換
+    img = img.convert("RGB")
+    transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))#((0.0),(1.0))
+    ])
+    img = transform(img)
+    x = img.reshape(1,3,224,224)#(1, 1, img_size, img_size)(1, 1, 100, 180)
+
+    #予測
+    net.eval()
+    y = net(x)
+
+    #結果を返す
+    y_prob = torch.nn.functional.softmax(torch.squeeze(y).cpu(), dim=0) #確率で表す
+    #y_prob = torch.nn.functional.softmax(torch.squeeze(y))#確率で表す
+    sorted_prob, sorted_indices = torch.sort(y_prob, descending = True)#降順にソート
+    return [(classes_ja[idx], classes_en[idx],prob.item()) for idx, prob in zip(sorted_indices, sorted_prob)]
