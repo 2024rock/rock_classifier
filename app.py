94920acb98bc7494f211f05e0bf502ac9a2c3f5a
2024rock <project2020tk@gmail.com>
2024-07-19 14:53:59 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -42,9 +42,10 @@ if img_file is not None:
 
         # 0％の確率を持つ要素を削除--------------------------------------------------
         # Filter out labels and probabilities with 0% probability
-        #filtered_labels = [label for label, prob in zip(pie_labels, pie_probs) if prob >= 0.0001]
-        filtered_probs = [prob for prob in pie_probs if prob >= 0.0001]
-        filtered_labels = [label for label, prob in zip(pie_labels, pie_probs) if prob >= 0.0001]
+        for result in results:
+            filtered_labels = [label for label, prob in zip(pie_labels, pie_probs) if prob >= 0.0001]
+            filtered_probs = [prob for prob in pie_probs if prob >= 0.0001]
+
         others_prob = sum(prob for prob in pie_probs if prob < 0.0001)
         if others_prob > 0:
              filtered_labels.append("others")
